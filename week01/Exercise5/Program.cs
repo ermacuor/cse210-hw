e953d870e39a52ae27d8f604f8596900311e3736
ermacuor <ermacuor@gmail.com>
2025-05-22 16:29:04 -0400

    added functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(UserNumber);
+        DisplayResult(UserName, squaredNumber);
+        
+    }
+    static void DisplayWelcome()
+    {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
 }
