0fced6c06c088bf060bf8dc097e654e85352db3c
ermacuor <ermacuor@gmail.com>
2025-05-22 13:22:04 -0400

    added variables

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        string letter = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        string symbol = "";
+        if (number % 10 >= 7)
+        {
+            symbol = "+";
+        }
+        else if (number % 10 < 3)
+        {
+            symbol = "-";
+        }
+
+        if (number >= 97)
+        {
+            Console.Write($"Your letter grade is {letter}.");
+        }
+        else if (number < 97)
+        {
+            Console.Write($"Your letter grade is {letter}{symbol}.");
+        }
+    }   
 }
