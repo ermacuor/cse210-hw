d9ce55d4713573a76cc9f2502024489ef668c70f
ermacuor <ermacuor@gmail.com>
2025-05-22 12:12:01 -0400

    Added a variables

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        string First = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string Last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last}, {First} {Last}");
     }
 }
